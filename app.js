172cd2b7fa6dfec8d2314763b9d816665c40c34d
eeian <80303355+eeian@users.noreply.github.com>
2022-08-15 21:30:40 +1200

    commit message

--- app.js
+++ app.js
@@ -15,14 +15,24 @@ connectDB();
 // cors
 app.use(cors({ origin: true, credentials: true }));
 
-// Init Middleware
-app.use(express.json({ extended: false }));
+// // Init Middleware
+// app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('Hello world!'));
+// app.get('/', (req, res) => res.send('Hello world!'));
+
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 // use Routes
 app.use('/api/books', books);
 
-const port = process.env.PORT || 8082;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
