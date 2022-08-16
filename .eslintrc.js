2f2cb17ddb0b6bbefda7b97a9c32f28bf855ae5b
eeian <80303355+eeian@users.noreply.github.com>
2022-08-16 19:43:54 +1200

    commit eslint and prettier changes

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+/* eslint-disable prettier/prettier */
+module.exports = {
+    root: true,
+    env: {
+      browser: true,
+      node: true
+    },
+    parserOptions: {
+      parser: 'babel-eslint',
+      ecmaVersion: 2018,
+      sourceType: 'module'
+    },
+    extends: [
+      'plugin:react/recommended',
+      'plugin:prettier/recommended'
+    ],
+    plugins: [],
+    // add your custom rules here
+    rules: {
+        "react/prop-types": 1
+    }
+  }
